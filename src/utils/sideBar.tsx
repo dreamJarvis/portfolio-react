9d8c6d5eb9c8a3f91180504937a764d09b6cba19
Tarun Jha <jhatarun16@gmail.com>
2021-07-19 06:18:23 +0530

    changes added

--- src/utils/sideBar.tsx
+++ src/utils/sideBar.tsx
@@ -8,29 +8,29 @@ export default function SideNavBar() {
 		<div>
 			<nav id='side_navbar'>
 				<section id='profile'>
-					<Link to='/'>
+					<Link to='/portfolio-react'>
 						<i className='fas fa-home'></i>
 					</Link>
 				</section>
 				<section id='options'>
 					<ul id='options_list'>
 						<li className='option-item'>
-							<Link to='/about'>
+							<Link to='/portfolio-react/about'>
 								<i className='fas fa-user'></i>
 							</Link>
 						</li>
 						<li className='option-item'>
-							<Link to='/projects'>
+							<Link to='/portfolio-react/projects'>
 								<i className='fas fa-terminal'></i>
 							</Link>
 						</li>
 						<li className='option-item'>
-							<Link to='/experience'>
+							<Link to='/portfolio-react/experience'>
 								<i className='fas fa-clipboard-list'></i>
 							</Link>
 						</li>
 						<li className='option-item'>
-							<Link to='contact'>
+							<Link to='/portfolio-react/contact'>
 								<i className='fas fa-envelope'></i>
 							</Link>
 						</li>
